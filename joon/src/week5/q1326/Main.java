9dc84a88245b18d8393362682440cbc5e6300a76
0BVer <dr0joon@gmail.com>
2022-11-17 02:13:24 +0900

    feat : sol 1392

--- joon/src/week5/q1326/Main.java
+++ joon/src/week5/q1326/Main.java
@@ -5,7 +5,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
