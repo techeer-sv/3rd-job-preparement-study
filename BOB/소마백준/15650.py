638ec4d991e17cf491d5248dee434de3592512d3
choiseyeon <choi20014830@gmail.com>
2023-02-06 02:35:45 +0900

    feat: 2/6 코테 - 1

---
+++ BOB/소마백준/15650.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+s = []
+visited = [False] * (n + 1)
+
+def dfs(start):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(start, n+1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs(i+1)
+        s.pop()
+        visited[i] = False
+        
+dfs(1)
