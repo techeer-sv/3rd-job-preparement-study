0c5b622bb25b91cc003bfa7c571efbe0dc6f43b4
choiseyeon <choi20014830@gmail.com>
2023-02-07 15:38:57 +0900

    feat: 2/7 코테 - 1

--- BOB/소마백준/15652.py
+++ BOB/소마백준/15652.py
@@ -1,19 +1,17 @@
 n, m = map(int, input().split())
 
 s = []
-visited = [False] * (n + 1)
+# visited = [False] * (n + 1)
 
 def dfs():
     if len(s) == m:
         print(' '.join(map(str, s)))
         return
-    for i in range(1, n+1):
-        if visited[i]:
+    for i in range(1, n + 1):
+        if len(s) != 0 and i < s[- 1]:
             continue
-        visited[i] = True
         s.append(i)
         dfs()
         s.pop()
-        visited[i] = False
         
 dfs()
